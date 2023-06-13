671465cd65b5c08087a2b2e328d6e009e0f66eb8
*твое <*>
2023-06-13 22:24:28 +0500

    change new hw 2-17

--- data.js
+++ data.js
@@ -1,14 +1,7 @@
 import { format } from "date-fns";
 
-export const now = (commentDate) => {
-    let createDate = format(new Date(), "yyyy-MM-dd hh.mm.ss");
-    let date = new Date();
-    let month = (date.getMonth() + 1).toString().padStart(2, '0');
-    let day = date.getDate().toString().padStart(2, '0');
-    let year = date.getFullYear().toString().substr(-2);
-    let hours = date.getHours().toString().padStart(2, '0');
-    let minutes = date.getMinutes().toString().padStart(2, '0');
-    const resultDate = `${year}.${month}.${day} ${hours}:${minutes}`;
-    return resultDate;
-}
+export const now = (date) => {
+    let formateDate = format(new Date(date), "yyyy-MM-dd hh.mm.ss");
+    return formateDate;
+};
 
